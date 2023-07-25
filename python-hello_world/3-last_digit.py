f1005fd221940b659a95e6e03b81a4a2d066cc1c
josephchigiz <josephochego@gmail.com>
2023-07-25 19:36:19 +0530

    push Last Digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if str(number)[-1] > '5' and number > 0:
-  print("Last digit of {} is {} and is greater than 5".format(number, str(number)[-1]))
-elif str(number)[-1] == '0':
-  print("Last digit of {} is {} and is 0".format(number, str(number)[-1]))
-elif str(number)[-1] < '6' and number != 0:
-  print("Last digit of {} is {} and is less than 6 and not 0".format(number, str(number)[-1]))
+lastDigit = number % 10
+if lastDigit > 5 and number > 0:
+  print("Last digit of {} is {} and is greater than 5".format(number, lastDigit))
+elif lastDigit == 0:
+  print("Last digit of {} is {} and is 0".format(number, lastDigit))
+else:
+  print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastDigit))
