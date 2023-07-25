c7a03ba17a74d8f0b790c68adb3b16c3bed0419d
josephchigiz <josephochego@gmail.com>
2023-07-25 19:28:00 +0530

    push 6-print_comb3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,5 +5,5 @@ if str(number)[-1] > '5' and number > 0:
   print("Last digit of {} is {} and is greater than 5".format(number, str(number)[-1]))
 elif str(number)[-1] == '0':
   print("Last digit of {} is {} and is 0".format(number, str(number)[-1]))
-else:
+elif str(number)[-1] < 6 and number < 0:
   print("Last digit of {} is {} and is less than 6 and not 0".format(number, str(number)[-1]))
