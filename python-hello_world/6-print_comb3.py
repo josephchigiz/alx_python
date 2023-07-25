9a723aafa33e5e65dd655b05357dc0e630ed43ba
josephchigiz <josephochego@gmail.com>
2023-07-25 19:23:49 +0530

    push 6-print_comb3

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
 for tens in range(10):
-  for ones in range(tens+1, 10):
-    print("{}{}".format(tens, ones), end=", " if ones != 9 and tens != 8 else "\n")
+  for ones in range(tens + 1, 10):
+    print("{}{}".format(tens, ones), end=", " if ones != 9 or tens != 8 else "\n")
