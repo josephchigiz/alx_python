4f96bd01f671c1ee8f3172f749e310ffccac8e48
josephchigiz <josephochego@gmail.com>
2023-07-25 20:58:00 +0530

    push Last Digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,8 +7,8 @@ if lastDigit < 0:
   lastDigit *= -1
   
 if lastDigit > 5:
-  print("Last digit of {} is {} and is greater than 5".format(number, lastDigit), end=" ")
+  print("Last digit of {} is {} and is greater than 5".format(number, lastDigit))
 elif lastDigit == 0:
-  print("Last digit of {} is {} and is 0".format(number, lastDigit), end=" ")
+  print("Last digit of {} is {} and is 0".format(number, lastDigit))
 else:
-  print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastDigit), end=" ")
+  print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastDigit))
