b0a7edaecfcbf8a3450a8e0e84a35b56d344faac
josephchigiz <josephochego@gmail.com>
2023-07-27 12:32:24 +0530

    Publish 0-sum.py

---
+++ python-functions/0-sum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def add(a, b):
+  """This function will add the values of  a and b and return the  sum."""
+  result = a + b
+  return result
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
+  
