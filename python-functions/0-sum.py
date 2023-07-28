b0ae4681bea79e66e5d7489a4e0accea00adb869
josephchigiz <josephochego@gmail.com>
2023-07-28 12:30:51 +0530

    Publish 0-sum.py

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,21 +1,5 @@
 #!/usr/bin/env python3
-def add(a, b):
-  
+def add(a, b): 
   return a + b
-  
-  match a, b:
-    case (1, 2):
-      print(add(1, 2))
-    case (98, 0):
-      print(add(98, 0))
-    case (100, -2):
-      print(add(100, -2))
-    case (0,0):
-      print(add(0, 0))
-      
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
-print(add(0, 0))
 
 
