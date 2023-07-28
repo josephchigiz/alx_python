9e052573066123f92b5e149947e8bf8f3a4239be
josephchigiz <josephochego@gmail.com>
2023-07-28 17:11:59 +0530

    Publish task 4

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,8 +1,12 @@
 def fibonacci_sequence(n):
   """This function will print the fibonacci sequence to the value of n"""
-  f_list = []
+  if n <= 0:
+        return []
+
+  f_list = [0]
   a, b = 0, 1
-  while a < n:
-    f_list.append(a)
-    a, b = b, a+b
+  while len(f_list) < n:
+    f_list.append(b)
+    a, b = b, a + b
+
   return f_list
