d86157aaae4564067788c580e9b960afa703de80
josephchigiz <josephochego@gmail.com>
2023-07-28 12:24:41 +0530

    Publish 0-sum.py

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-# def add(a, b):
-#   """This function adds a and b and returns the result"""
-#   add = __import__('0-sum').add
-#   return add(a, b)
-
-# print(add(1, 2))
-# print(add(98, 0))
-# print(add(100, -2))
-
 def add(a, b):
+  
   return a + b
-
+  
+  match a, b:
+    case (1, 2):
+      print(add(1, 2))
+    case (98, 0):
+      print(add(98, 0))
+    case (100, -2):
+      print(add(100, -2))
+    case (0,0):
+      print(add(0, 0))
+      
 print(add(1, 2))
 print(add(98, 0))
 print(add(100, -2))
+print(add(0, 0))
+
+
