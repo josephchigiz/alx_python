c5784398ca5ff603e7378717a03dc26320db972f
josephchigiz <josephochego@gmail.com>
2023-07-28 12:32:23 +0530

    Publish task 2

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,9 +1,3 @@
 def pow(a, b):
   """This function computes a to the power of b and returns the value"""
   return a ** b
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -3,10 +3,5 @@
 def convert_to_celsius(fahrenheit):
   """This function converts the given temperature from fahrenheit to celsius"""
   f = int(fahrenheit)
-  celsius = 5/9*(f-32)
+  celsius = ((5/9)*(f-32))
   return celsius
-
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
