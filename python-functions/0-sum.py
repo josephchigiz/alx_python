cd95cc8a71bba0e1d0bc5108687300135cdb8f71
josephchigiz <josephochego@gmail.com>
2023-07-28 11:23:00 +0530

    Publish 1-power.py

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-#add = __import__('0-sum').add
+# def add(a, b):
+#   """This function adds a and b and returns the result"""
+#   add = __import__('0-sum').add
+#   return add(a, b)
+
+# print(add(1, 2))
+# print(add(98, 0))
+# print(add(100, -2))
 
 def add(a, b):
-  """This function will add the values of  a and b and return the  sum."""
   return a + b
 
 print(add(1, 2))
 print(add(98, 0))
 print(add(100, -2))
-  
