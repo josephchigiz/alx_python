80f1bd0776c7ad9b963ca2cb4649023281ed63a8
josephchigiz <josephochego@gmail.com>
2023-07-28 12:38:05 +0530

    Publish task 2

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -2,6 +2,4 @@
 
 def convert_to_celsius(fahrenheit):
   """This function converts the given temperature from fahrenheit to celsius"""
-  f = int(fahrenheit)
-  celsius = ((5/9)*(f-32))
-  return celsius
+  return celsius ((fahrenheit - 32) * (5 / 9))
