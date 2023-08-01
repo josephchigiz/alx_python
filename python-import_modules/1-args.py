94f817cbb0e4a9ba4a17efacf9828a4ef0a55cee
josephchigiz <josephochego@gmail.com>
2023-08-01 18:59:05 +0530

    push 0-add

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,18 @@
+import sys
+
+def listArgs():
+  no_arg = len(sys.argv) -1
+  ls_arg = sys.argv[1:]
+  
+  if no_arg == 1:
+    print("{} argument:".format(no_arg))
+  elif no_arg > 1:
+    print("{} arguments:".format(no_arg))
+  else:
+    print("O arguments.")
+    
+  for num, arg in enumerate(ls_arg, start=1):
+    print("{}: {}".format(num, arg))
+    
+if __name__ == "__main__":
+  listArgs()
