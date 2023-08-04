6adc3503788075c6b52666b9e621e16f2005edfa
josephchigiz <josephochego@gmail.com>
2023-08-04 11:36:44 +0530

    push python-data_structures

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,12 +1,23 @@
+# def print_matrix_integer(matrix=[[]]):
+#     if not matrix:
+#         print(end="\n")
+#         return
+    
+#     for row in matrix:
+#         for k in range(len(row)):
+#             if k < len(row) - 1:
+#                 print("{:d}".format(row[k]), end=" ")
+#             else:
+#                 print("{:d}".format(row[k]))
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print(end="\n")
+        print()
         return
     
     for row in matrix:
-        for k in range(len(row)):
-            if k < len(row) - 1:
-                print("{:d}".format(row[k]), end=" ")
-            else:
-                print("{:d}".format(row[k]))
-                
+        row_length = len(row)
+        for m in range(row_length):
+            if m!= 0:
+                print(" ", end="")
+            print("{:d}".format(row[m]), end="")
+        print()
