6c83f9d159c9148c3cb8c1992ad0f2eed0eadf41
josephchigiz <josephochego@gmail.com>
2023-08-08 12:22:21 +0530

    push python-more_data_structures

--- python-more_data_structures/try.py
+++ python-more_data_structures/try.py
@@ -1,6 +1,22 @@
-common_elements = __import__('1-common_elements').common_elements
+#!/usr/bin/python3
+update_dictionary = __import__('2-update_dictionary').update_dictionary
 
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
+def print_sorted_dictionary(my_dict):
+    """ Print sorted dictionary """
+    keys = sorted(my_dict.keys())
+    for k in keys:
+        print("{}: {}".format(k, my_dict[k]))
+
+a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+new_dict = update_dictionary(a_dictionary, 'language', "Python")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+print("--")
+print("--")
+
+new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
