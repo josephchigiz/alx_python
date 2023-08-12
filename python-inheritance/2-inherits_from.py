c4c6eba3bff510427a24e72b9ee41176a44a1f17
josephchigiz <josephochego@gmail.com>
2023-08-12 23:21:56 +0530

    push task-2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -4,9 +4,5 @@
 def inherits_from(obj, a_class):
     """This returns true if the object an instance of, or if the object is an
     instance of a class that inherited from, the specified class"""
-    some_class = type(obj)
 
-    if isinstance(obj, a_class) == True or issubclass(some_class, a_class) == True:
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class) or issubclass(type(obj), a_class)
--- python-inheritance/try.py
+++ python-inheritance/try.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-is_kind_of_class = __import__("1-is_kind_of_class").is_kind_of_class
+inherits_from = __import__("2-inherits_from").inherits_from
 
 a = 1
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
-if is_kind_of_class(a, float):
-    print("{} comes from {}".format(a, float.__name__))
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+print(inherits_from(a, int))
