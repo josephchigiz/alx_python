e84156a996165b8fbab0091f8757a402b30774f9
josephchigiz <josephochego@gmail.com>
2023-08-12 22:39:01 +0530

    push task-0

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -2,6 +2,8 @@
 
 
 def is_kind_of_class(obj, a_class):
+    """This returns true if the object an instance of, or if the object is an
+    instance of a class that inherited from, the specified class"""
     if issubclass(obj, a_class) == True:
         return True
     else:
