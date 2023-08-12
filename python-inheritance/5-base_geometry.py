21f1ae9b67a17747895d75fe1d3990711a8dcab1
josephchigiz <josephochego@gmail.com>
2023-08-13 00:10:52 +0530

    push task-5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+"""Task 4"""
+
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name="", value=0):
+        self.name = name
+        self.value = value
+
+        if not type(value) is int:
+            raise TypeError("{} must be an integer".format(self.name))
+        elif self.value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
