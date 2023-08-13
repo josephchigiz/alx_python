687e0b93ecac4fb547f82324819f0e75db965ace
josephchigiz <josephochego@gmail.com>
2023-08-13 19:12:33 +0530

    push tasks

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -53,6 +53,8 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
+    """Square Class"""
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
