509ca8f29dde0f8d54b75b0d7f0f0c4e87a4e0e7
josephchigiz <josephochego@gmail.com>
2023-08-13 22:01:35 +0530

    push tasks

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,24 +1,25 @@
-"""Base"""
+"""Base Module
+
+Defines the Base class for creating objects with unique IDs.
+"""
 
 
 class Base:
-    """attribute __nb_objects = 0 is private"""
+    """Base Class
 
-    __nb_objects = 0
-    """ 
-    ---
+    Creates objects with unique IDs.
     """
 
+    __nb_objects = 0
+
     def __init__(self, id=None):
-        """Used to avoid duplication by managing id"""
+        """Initialize a Base object.
+
+        Args:
+            id (int, optional): Optional ID for the object.
+        """
         if id is not None:
-            """
-            ---
-            """
             self.id = id
         else:
-            """
-            ---
-            """
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
