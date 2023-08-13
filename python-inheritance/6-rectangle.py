02cde5ff064774f8316f7fed38541584440c3da4
josephchigiz <josephochego@gmail.com>
2023-08-13 20:52:25 +0530

    push tasks

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -14,23 +14,20 @@ class BaseGeometryMeta(type):
 class BaseGeometry(metaclass=BaseGeometryMeta):
     """BaseGeometry Class"""
 
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name="", value=0):
-        self.__name = name
-        self.__value = value
-
-        if not type(value) is int:
-            raise TypeError("{} must be an integer".format(self.__name))
-        elif self.__value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.__name))
-
     def __dir__(self) -> None:
         """This will control inherited attribute access"""
         attributes = super().__dir__()
         used_attr = [att for att in attributes if att != "__init_subclass__"]
         return used_attr
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
@@ -39,9 +36,3 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.__width = super().integer_validator("width", width)
         self.__height = super().integer_validator("height", height)
-
-    def __dir__(self) -> None:
-        """This will control inherited attribute access"""
-        attributes = super().__dir__()
-        used_attr = [att for att in attributes if att != "__init_subclass__"]
-        return used_attr
