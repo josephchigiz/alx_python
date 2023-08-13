1e2ae78cfc7e9131b223a052754b6b52c6d12e43
josephchigiz <josephochego@gmail.com>
2023-08-13 21:12:27 +0530

    push tasks

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -3,7 +3,4 @@
 
 def is_same_class(obj, a_class):
     """This returns true if the object is exactly an instance of the specified class"""
-    if isinstance(obj, a_class) == True:
-        return True
-    else:
-        return False
+    return type(obj) is a_class
