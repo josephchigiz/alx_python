7644ee64c5ab308c6678a56c154f0ddaab534314
josephchigiz <josephochego@gmail.com>
2023-08-13 20:09:04 +0530

    push tasks

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,16 @@
 """Task 3"""
 
 
-class BaseGeometry:
+class BaseGeometryMeta(type):
+    """Meta Class for BAseGeometry"""
+
+    def __dir__(self):
+        attributes = super().__dir__()
+        used_attr = [att for att in attributes if att != "__init_subclass__"]
+        return used_attr
+
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
     """This is a BaseGeometry Class"""
 
     def __dir__(self):
