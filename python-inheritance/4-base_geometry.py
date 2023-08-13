51c2e1e30ef71aa743f2fc4d1e6f55027034d283
josephchigiz <josephochego@gmail.com>
2023-08-13 18:04:18 +0530

    push task-4

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,6 +1,16 @@
 #!/usr/bin/Python3
+"""TASK 4"""
+
+
 class BaseGeometry:
     """BaseGeometry Class"""
+
+    def __dir__(self) -> None:
+        """This will control inherited attribute access"""
+        attributes = super().__dir__(BaseGeometry)
+        return [
+            attribute for attribute in attributes if attribute != "__init_subclass__"
+        ]
 
     def area(self):
         raise Exception("area() is not implemented")
