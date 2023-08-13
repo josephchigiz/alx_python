19ce227903e8caed348e4b1a240c6e21b899bedc
josephchigiz <josephochego@gmail.com>
2023-08-13 21:21:32 +0530

    push tasks

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,32 +1,32 @@
 """Task 6"""
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
+
+# class BaseGeometryMeta(type):
+#     """Meta Class for BAseGeometry"""
+
+#     def __dir__(self):
+#         attributes = super().__dir__()
+#         used_attr = [att for att in attributes if att != "__init_subclass__"]
+#         return used_attr
 
 
-class BaseGeometryMeta(type):
-    """Meta Class for BAseGeometry"""
+# class BaseGeometry(metaclass=BaseGeometryMeta):
+#     """BaseGeometry Class"""
 
-    def __dir__(self):
-        attributes = super().__dir__()
-        used_attr = [att for att in attributes if att != "__init_subclass__"]
-        return used_attr
+#     def __dir__(self) -> None:
+#         """This will control inherited attribute access"""
+#         attributes = super().__dir__()
+#         used_attr = [att for att in attributes if att != "__init_subclass__"]
+#         return used_attr
 
+#     def area(self):
+#         raise Exception("area() is not implemented")
 
-class BaseGeometry(metaclass=BaseGeometryMeta):
-    """BaseGeometry Class"""
-
-    def __dir__(self) -> None:
-        """This will control inherited attribute access"""
-        attributes = super().__dir__()
-        used_attr = [att for att in attributes if att != "__init_subclass__"]
-        return used_attr
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+#     def integer_validator(self, name, value):
+#         if not isinstance(value, int):
+#             raise TypeError("{} must be an integer".format(name))
+#         elif value <= 0:
+#             raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
