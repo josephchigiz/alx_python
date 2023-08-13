9d610d31171affff0dba19803e5f84e99b0b6f63
josephchigiz <josephochego@gmail.com>
2023-08-13 18:13:07 +0530

    push tasks

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,19 +1,9 @@
 """Task 3"""
-from collections.abc import Iterable
 
 
 class BaseGeometry:
     """This is a BaseGeometry Class"""
 
-    # def __dir__(self) -> None:
-    #     """This will control inherited attribute access"""
-    #     attributes = super().__dir__()
-    #     used_attr = []
-
-    #     for att in attributes:
-    #         if att != "__init_subclass__":
-    #             used_attr.append(att)
-    #     return used_attr
     def __dir__(self) -> None:
         """This will control inherited attribute access"""
         attributes = super().__dir__()
