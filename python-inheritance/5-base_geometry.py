0048fbba9926b5755fff71aa86f30a53030cc447
josephchigiz <josephochego@gmail.com>
2023-08-13 21:00:28 +0530

    push tasks

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,8 +1,16 @@
-#!/usr/bin/Python3
 """tASK 5"""
 
 
-class BaseGeometry:
+class BaseGeometryMeta(type):
+    def __dir__(self) -> None:
+        """This will control inherited attribute access"""
+        attributes = super().__dir__()
+        return [
+            attribute for attribute in attributes if attribute != "__init_subclass__"
+        ]
+
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
     """BaseGeometry Class"""
 
     def __dir__(self) -> None:
@@ -15,8 +23,8 @@ class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name="", value=0):
-        if not type(value) is int:
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
