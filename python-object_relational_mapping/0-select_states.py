c1e422e608fd711953291ef336a7f7023da3b778
joesphchigiz <joesphochego@gmail.com>
2023-09-06 12:10:57 +0530

    ORM Tasks

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -31,24 +31,10 @@ def states_list(username, password, db_name):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <db_name>")
-    else:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        db_name = sys.argv[3]
-        states_list(username, password, db_name)
-
-# username = root
-# password = root
-# database = hbtn_0e_0_usa
-
-
-# cur = dabase.cursor()
-# cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-# query_rows = cur.fetchall()
-# for row in query_rows:
-#     print(row)
-# cur.close()
-# dabase.close()
+    # if len(sys.argv) != 4:
+    #     print("Usage: python script.py <username> <password> <db_name>")
+    # else:
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    states_list(username, password, db_name)
