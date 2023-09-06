a09dd2aeb1cad7bfaf9b3f11a66ddffbf3c2f2f0
joesphchigiz <joesphochego@gmail.com>
2023-09-06 11:49:51 +0530

    ORM Tasks

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,44 @@
 import MySQLdb as DB
 
+
+def states_list(username, password, db_name):
+    try:
+        dabase = DB.connect(
+            host="localhost",
+            port="3306",
+            user="root",
+            passwd="root",
+            db="hbtn_0e_0_usa",
+        )
+
+        cur = dabase.cursor()
+
+        list = "SELECT * FROM states ORDER BY id ASC"
+
+        cur.execute(list)
+
+        states = cur.fetchall()
+
+        for state in states:
+            print(state)
+
+    except DB.Error:
+        print()
+    finally:
+        cur.close()
+        dabase.close()
+
+
 # username = root
 # password = root
 # database = hbtn_0e_0_usa
 
-dabase = DB.connect(
-    host="localhost", port="3306", user="root", passwd="root", db="hbtn_0e_0_usa"
-)
 
-cur = dabase.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
+# cur = dabase.cursor()
+# cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-dabase.close()
+# query_rows = cur.fetchall()
+# for row in query_rows:
+#     print(row)
+# cur.close()
+# dabase.close()
