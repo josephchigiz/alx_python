d1f4795670a6f46409ac8c32ce03909f3fb9a8c1
joesphchigiz <joesphochego@gmail.com>
2023-09-06 12:43:10 +0530

    ORM Tasks

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+import MySQLdb as DB
+import sys
+
+
+def states_list(username, password, db_name, new_state):
+    try:
+        dabase = DB.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name,
+        )
+
+        cur = dabase.cursor()
+
+        list = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+        cur.execute(list, (new_state,))
+
+        states = cur.fetchall()
+
+        for state in states:
+            print(state)
+
+    except DB.Error:
+        print()
+    finally:
+        cur.close()
+        dabase.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    states_list(username, password, db_name)
