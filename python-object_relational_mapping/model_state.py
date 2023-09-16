af488b5af68d01ba9f68d508c306c4aa03d4f975
joesphchigiz <joesphochego@gmail.com>
2023-09-16 20:10:13 +0530

    ORM Tasks

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,26 +1,20 @@
-#!/usr/bin/python3
-"""
-This script defines base and 
-state classes to work with SQLAlchemt ORM
-"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
+"""
+The declarative base.
 
-class States(Base):
+It is used as a parent class for SQLAlchemy ORM classes to define database tables.
+"""
+class State(Base):
     """
     Represents a state in the database.
 
     Attributes:
-        id (int): The unique identifier for the state (auto-generated, non-null, primary key).
-        name (str): The name of the state (non-null, max length of 128 characters).
+        id (int):
+        name (str): 
     """
     __tablename__ = 'states'
-
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-
-    name = Column(String(128), nullable=False)
-
-
+    name =  Column(String(128), nullable=False)
