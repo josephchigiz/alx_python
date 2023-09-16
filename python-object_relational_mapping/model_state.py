7f236f562d050ad72e396243e17a9b891093726f
joesphchigiz <joesphochego@gmail.com>
2023-09-16 18:31:18 +0530

    ORM Tasks

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,23 +4,29 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, INT, autoincrement
 from sqlalchemy.orm import sessionmaker
 
+
+Base = declarative_base()
+"""docstring for declarative class"""
+
+class State(Base):
+     """docstring for State"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
 username = 'root'
 password = 'root'
 port = 3306
 database = 'hbtn_0e_6_usa'
 
-path = 'mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database)
+path = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database)
 
 dabase = create_engine(path)
+       
 
-Base = declarative_base
+Base.metadata.create_all(dabase)
 
-class State(object):
-     """docstring for State"""
-    __tablename__ = 'states'
-
-    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
-
-        
-
+Session = sessionmaker(bind=dabase)
+session = Session()
