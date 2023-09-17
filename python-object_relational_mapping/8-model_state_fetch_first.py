54233f3283894fb88d146040f2bb8ec2273ffb97
joesphchigiz <joesphochego@gmail.com>
2023-09-17 21:46:03 +0530

    ORM Tasks

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    # path = "mysql+mysqldb://{}:{}@localhost/{}".format(
+    #     argv[1],
+    #     argv[2],
+    #     argv[3])
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        argv[1],
+        argv[2],
+        argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id[1], state.name[1]))
+    session.close()
