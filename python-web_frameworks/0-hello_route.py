73992d74c5edb4fd70388a7673b4ac242689e0a2
josephchigiz <josephochego@gmail.com>
2023-10-05 15:02:12 +0530

    Task 0

---
+++ python-web_frameworks/0-hello_route.py
@@ -0,0 +1,15 @@
+# Basic flask app
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    # This is the default route
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
