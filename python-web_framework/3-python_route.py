ffe8642d0c2b981fb4b6b2b891d40e3cfcceadd4
josephchigiz <josephochego@gmail.com>
2023-10-05 16:15:52 +0530

    Task 2

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,46 @@
+"""Basic flask app"""
+from flask import Flask
+
+"""The module imported handles all the framework's operations, from routing to connecting temps."""
+
+app = Flask(__name__)
+
+"""Routing"""
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    This is the default route
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    This is the /hbnb route
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    This is the /c route
+    """
+    text_spaces = text.replace("_", " ")
+    return f"C {text_spaces}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def py_text(text="is cool"):
+    """
+    This is the /python route
+    """
+    text_spaces = text.replace("_", " ")
+    return f"Python {text_spaces}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
