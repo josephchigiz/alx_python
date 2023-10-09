7cbf054e445db5407d524a112526f0f14fe8db23
josephchigiz <josephochego@gmail.com>
2023-10-09 18:49:46 +0530

    update Task 6

---
+++ python-web_framework/6-number_odd_or_even.py
@@ -0,0 +1,74 @@
+"""Basic flask app"""
+from flask import Flask, render_template
+
+"""The module imported handles all the framework's operations, from routing to connecting temps."""
+
+app = Flask(__name__)
+
+"""Routing"""
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    This is the default route
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    This is the /hbnb route
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    This is the /c route
+    """
+    text_spaces = text.replace("_", " ")
+    return f"C {text_spaces}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_text(text="is cool"):
+    """
+    This is the /python route
+    """
+    text_spaces = text.replace("_", " ")
+    return f"Python {text_spaces}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def n_int(n):
+    """
+    This is the /number route
+    """
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """
+    This will route to the number_temp which will be displayed
+    """
+    return render_template("5-number.html", number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    This will route to the number_temp which will be displayed
+    """
+    if n % 2 == 0:
+        return render_template("6-number_odd_or_even.html", number=n, type="even")
+    else:
+        return render_template("6-number_odd_or_even.html", number=n, type="odd")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
