72bbfd3b867c58f8d726392e279f8702b0b598ff
josephchigiz <josephochego@gmail.com>
2023-10-25 20:10:41 +0530

    push Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,2 +1,34 @@
 import requests
 
+def employee_todo_progress(employee_id):
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print(f"Could not get employee information.")
+        return
+    
+    user_data = user_response.json()
+    employee_name = user_data["name"]
+    
+    todos_response = requests.get(todos_url)
+    if todos_response.status_code != 200:
+        print(f"Could not get the employee's TODO list.")
+        return
+    
+    todos_data = todos_response.json()
+    
+    completed_tasks = [todo for todo in todos_data if todo["completed"]]
+    num_completed_tasks = len(completed_tasks)
+    total_tasks = len(todos_data)
+    
+    print(f"Employe {employee_id} has completed ({num_completed_tasks}/{total_tasks}):")
+    
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
+        
+if __name__ == "__main__":
+    employee_id = int(input("Enter the employee ID: "))
+    get_employee_todo_progress(employee_id)
+    
