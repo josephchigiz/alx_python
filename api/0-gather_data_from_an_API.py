c50dcf93ef3b0f585c36ad85b42039938b70421e
josephchigiz <josephochego@gmail.com>
2023-10-25 20:42:00 +0530

    push Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -24,7 +24,7 @@ def employee_todo_progress(employee_id):
     num_completed_tasks = len(completed_tasks)
     total_tasks = len(todos_data)
     
-    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
     
     for task in completed_tasks:
         print(f"\t{task['title']}")
