a097985a1657ad18865318b9f96e94adebb58c66
josephchigiz <josephochego@gmail.com>
2023-10-26 16:44:31 +0530

    push Task 2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python
+
+"""import json and requests"""
 import json
 import requests
-#initialize empty dict
+
+"""
+initialize empty dict
+"""
 all_tasks = {}
 
-# loop through user IDs from 1 to 10
+"""loop through user IDs from 1 to 10"""
 for user_id in range(1, 11):
-    # send get request to receive the TODO list for specified user
+    """send get request to receive the TODO list for specified user"""
     request = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
-    #parse JSON data from the response
+    """parse JSON data from the response"""
     data = request.json()
 
+    """Initialize an empty list to store tasks for this"""
     tasks = []
 
-    #iterate through each TODO item and structure the data
+    """iterate through each TODO item and structure the data"""
     for item in data:
-        # #
+        """Append a dictionary task information to the task lists"""
         tasks.append({
             "username": f"USER_{user_id}",
             "task": item.get('title'),
             "completed": item.get('completed')
         })
 
-    #add user's tasks to all_tasks dictionary
+    """add user's tasks to all_tasks dictionary"""
     all_tasks[str(user_id)] = tasks
 
-# JSON file name
+"""JSON file name"""
 json_file = "USER_ID.json"
 
-#open JSON file for writing
+"""open JSON file for writing"""
 with open(json_file, mode='w') as file:
-    # write collected data to JSON file
+    """write collected data to JSON file"""
     json.dump(all_tasks, file)
