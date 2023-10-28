0fe596dcd9cb9a0f471fc2b213c479f7115ffe57
josephchigiz <josephochego@gmail.com>
2023-10-28 14:21:40 +0530

    push Task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,11 +1,47 @@
+# import csv
+# import requests
+# import sys
+
+# id = str(sys.argv[1])
+
+# request_user = requests.get('https://jsonplaceholder.typicode.com/users/'+id)
+# request_todos = requests.get('https://jsonplaceholder.typicode.com/users/'+id+'/todos')
+
+# data_user = request_user.json()
+# data_todos = request_todos.json()
+
+# completed = 0
+
+# tasks_data = []
+
+# for i in data_todos:
+#     if i.get('completed')==True:
+#         completed = completed + 1
+#     tasks_data.append([id, data_user.get('name'), i.get('completed'), i.get('title')])
+
+# print ('Employee {} is done with tasks({}/{}):'.format(data_user.get('name'), completed,len(data_todos)))
+
+# for item in data_todos:
+#     if item.get('completed') == True:
+#         print('\t ' + item.get('title'))
+
+# csv_file_name = f'{id}.csv'
+
+# with open(csv_file_name, mode='w', newline='') as file:
+#     csv_writer = csv.writer(file)
+#     csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+#     csv_writer.writerows(tasks_data)
+
 import csv
 import requests
 import sys
 
-id = str(sys.argv[1])
+id = sys.argv[1]
 
-request_user = requests.get('https://jsonplaceholder.typicode.com/users/'+id)
-request_todos = requests.get('https://jsonplaceholder.typicode.com/users/'+id+'/todos')
+request_user = requests.get("https://jsonplaceholder.typicode.com/users/" + id)
+request_todos = requests.get(
+    "https://jsonplaceholder.typicode.com/users/" + id + "/todos"
+)
 
 data_user = request_user.json()
 data_todos = request_todos.json()
@@ -15,19 +51,25 @@ completed = 0
 tasks_data = []
 
 for i in data_todos:
-    if i.get('completed')==True:
+    if i.get("completed") == True:
         completed = completed + 1
-    tasks_data.append([id, data_user.get('name'), i.get('completed'), i.get('title')])
+    tasks_data.append(
+        [id, data_user.get("username"), i.get("completed"), i.get("title")]
+    )
 
-print ('Employee {} is done with tasks({}/{}):'.format(data_user.get('name'), completed,len(data_todos)))
+print(
+    "Employee {} is done with tasks({}/{}):".format(
+        data_user.get("name"), completed, len(data_todos)
+    )
+)
 
 for item in data_todos:
-    if item.get('completed') == True:
-        print('\t ' + item.get('title'))
+    if item.get("completed") == True:
+        print("\t " + item.get("title"))
 
-csv_file_name = f'{id}.csv'
+file_name = f"{id}.csv"
 
-with open(csv_file_name, mode='w', newline='') as file:
-    csv_writer = csv.writer(file)
-    csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-    csv_writer.writerows(tasks_data)
+with open(file_name, "w") as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+    csvwriter.writerows(tasks_data)
